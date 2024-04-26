a81d49bc45449e0715599b0e32c0070734e1b293
PS2023KRGKSNXN\Admin <wangtao112358@live.com>
2024-04-26 17:32:53 +0800

    support generic

--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,7 @@
+use core::fmt;
+use fmt::Formatter;
+use std::str::FromStr;
+
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -13,13 +17,22 @@ pub enum Subcommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short,long,value_parser=verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)] //, default_value = "output.json"
+    pub output: Option<String>,
+
+    #[arg(short, long,value_parser=parse_format, default_value ="json")]
+    pub format: OutputFormat,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
@@ -28,10 +41,40 @@ pub struct CsvOpts {
     pub header: bool,
 }
 
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format: {}", s)),
+        }
+    }
+}
+
 fn verify_input_file(filename: &str) -> Result<String, String> {
     if std::path::Path::new(filename).exists() {
         Ok(filename.into())
     } else {
-        Err("File does not exist".into())
+        Err(format!("File {} does not exist", filename)) // 使用 format! 宏
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
